8b8b134f99fd8f4a1722147fe84f13c7645c027d
Abhishek Choudhari <abhishekc97@gmail.com>
2023-03-13 21:18:51 +0530

    job card can copy job link, and redirect to about page

--- src/components/JobCard/JobCard.jsx
+++ src/components/JobCard/JobCard.jsx
@@ -1,83 +1,99 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import styles from "./JobCard.module.css";
 import TimeAgo from "timeago-react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import EditJob from "../EditJob/EditJob.jsx";
 
 export default function JobCard({ job, handleJobPostUpdated, toastAlert }) {
     const [showEditJobModal, setShowEditJobModal] = useState(false);
-    const [jobData, setJobData] = useState();
+
+    // getting the complete URL href
     let url = window.location.href;
     let copylink = url + `job/${job._id}`;
 
-    useEffect(() => {
-        setJobData(job);
-    }, [job]);
+    useEffect(() => {}, [job]);
 
     function handleCopyLink() {
         navigator.clipboard.writeText(copylink);
         toastAlert();
     }
 
+    const navigate = useNavigate();
+
+    function handleSelectJobPost() {
+        navigate(`/job/${job._id}`);
+    }
+
     return (
-        <div className={styles.jobCard}>
-            {/* <Link to={`/job/:${job.id}`}> */}
-            <div className={styles.cardLeft}>
-                <div className={styles.companyLogo}>
-                    <img
-                        className={styles.companyLogo}
-                        src={job.companyLogo}
-                        alt=""
-                    />
-                </div>
-            </div>
-            <div className={styles.cardRight}>
-                <div className={styles.rightTop}>
-                    <span className={styles.jobTitle}>{job.jobPosition}</span>
-                    <div className={styles.postedTime}>
-                        <TimeAgo datetime={job.createdAt} locale="en_US" />
-                    </div>
-                </div>
-                <div className={styles.rightMiddle}>
-                    <div className={styles.numberOfEmployees}>
-                        <span className={styles.employeeCountIcon}></span>
-                        <span>{job.employeeCount}</span>
-                    </div>
-                    <div className={styles.salary}>
-                        <span className={styles.salaryIcon}></span>
-                        <span>{job.monthlySalary}</span>
-                    </div>
-                    <div className={styles.location}>
-                        <span className={styles.locationIcon}></span>
-                        <span>{job.jobLocation}</span>
-                    </div>
-                </div>
-                <div className={styles.rightBottom}>
-                    <div className={styles.jobMode}>{job.jobMode}</div>
-                    <div className={styles.contractType}>{job.jobType}</div>
-                    {showEditJobModal && (
-                        <EditJob
-                            job={job}
-                            show={showEditJobModal}
-                            onClose={setShowEditJobModal}
-                            handleJobPostUpdated={handleJobPostUpdated}
+        <>
+            <div className={styles.jobCard}>
+                <div className={styles.cardLeft}>
+                    <div className={styles.companyLogo}>
+                        <img
+                            className={styles.companyLogo}
+                            src={job.companyLogo}
+                            alt=""
                         />
-                    )}
-                    <button
-                        className={styles.editJob}
-                        onClick={() => setShowEditJobModal(true)}
+                    </div>
+                </div>
+                <div className={styles.cardRight}>
+                    <div
+                        className={styles.rightTop}
+                        onClick={handleSelectJobPost}
                     >
-                        Edit Job
-                    </button>
-                    <button
-                        className={styles.copyLink}
-                        onClick={handleCopyLink}
-                    >
-                        Copy Link
-                    </button>
+                        <span className={styles.jobTitle}>
+                            {job.jobPosition}
+                        </span>
+                        <div className={styles.postedTime}>
+                            <TimeAgo datetime={job.createdAt} locale="en_US" />
+                        </div>
+                    </div>
+                    <div className={styles.rightMiddle}>
+                        <div className={styles.numberOfEmployees}>
+                            <span className={styles.employeeCountIcon}></span>
+                            <span>{job.employeeCount}</span>
+                        </div>
+                        <div className={styles.salary}>
+                            <span className={styles.salaryIcon}></span>
+                            <span>{job.monthlySalary}</span>
+                        </div>
+                        <div className={styles.location}>
+                            <span className={styles.locationIcon}></span>
+                            <span>{job.jobLocation}</span>
+                        </div>
+                    </div>
+                    <div className={styles.rightBottom}>
+                        <div className={styles.jobMode}>{job.jobMode}</div>
+                        <div className={styles.contractType}>{job.jobType}</div>
+                        {showEditJobModal && (
+                            <EditJob
+                                job={job}
+                                show={showEditJobModal}
+                                onClose={setShowEditJobModal}
+                                handleJobPostUpdated={handleJobPostUpdated}
+                            />
+                        )}
+                        <button
+                            className={styles.editJob}
+                            onClick={(e) => {
+                                e.stopPropagation();
+                                setShowEditJobModal(true);
+                            }}
+                        >
+                            Edit Job
+                        </button>
+                        <button
+                            className={styles.copyLink}
+                            onClick={(e) => {
+                                e.stopPropagation();
+                                handleCopyLink();
+                            }}
+                        >
+                            Copy Link
+                        </button>
+                    </div>
                 </div>
             </div>
-            {/* </Link> */}
-        </div>
+        </>
     );
 }
